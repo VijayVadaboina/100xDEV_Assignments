994eca7755741a89e6b1a1c0b4f8cd0d075423bd
VijayVadaboina <vijayvvadaboina@gmail.com>
2024-09-21 20:22:53 +0530

    added skeleton routes for FirstApp

--- WEBDEV/WEEK8/FirstApp/index.js
+++ WEBDEV/WEEK8/FirstApp/index.js
@@ -1,1 +1,30 @@
+const express = require("express");
+const app = express();
 
+app.post("/user/signup", (req, res) => {
+  res.json({
+    message: " signed up successfully",
+  });
+});
+app.post("/user/signin", (req, res) => {
+  res.json({
+    message: " signed in successfully",
+  });
+});
+app.post("/user/purchase", (req, res) => {
+  res.json({
+    message: " purchase the course",
+  });
+});
+app.post("/course.purchase", (req, res) => {
+  res.json({
+    message: " get all the course",
+  });
+});
+app.get("/Courses", (req, res) => {
+  res.json({
+    message: " get all the course",
+  });
+});
+
+app.listen(3000);
